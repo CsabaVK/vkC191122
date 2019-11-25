a67a814b4921a8ba4d180cff2e900972ee30e994
Csaba <56678429+CsabaVK@users.noreply.github.com>
2019-11-25 10:01:08 +0100

    first php

---
+++ 1922PHP/command.md
@@ -0,0 +1,20 @@
+Commands:
+$GLOBAL
+    $_SERVER
+    $_GET
+    $_POST
+    $_COOKIE
+    $_SESSION
+
+<!-- tanulj már meg írni basszalakmeg -->
+1. H#P
+2. URL
+3. Üzenettörzs
+
+KIMENET:
+<!DOCTYPE HTML>
+<?PHP
+    $a = 12;
+    echo "alma";
+?>
+<div class="<?='alma'?>">
---
+++ 1922PHP/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo 'asd' ;
+    $d = 12; //
+    $o = 014; //
+    $h = 0xC; //hexa
+    $b = 0b1100; // bináris
+
+    echo $d;
+    ?>
+
+    <h1>
+        <?php 
+        echo "Ez egy egyes címsor, ".$b; 
+
+        $s = "Valami szöveg\n";
+
+        $cucc = "alma";
+
+        $ss = "piros{$cucc}\n<br>";
+        echo $ss;
+        print "$s";
+        
+        ?>
+        </h1>
+    <!--  -->
+</body>
+</html>
